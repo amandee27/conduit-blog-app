71ad5e9f21a269c5e7184d4e4bc4390322faae92
Amandee Ellawala <amandee.manushika.e@gmail.com>
2021-07-17 23:43:49 +0530

    Remove comment

--- conduit-blog-app/src/app/sign-in/sign-in.component.ts
+++ conduit-blog-app/src/app/sign-in/sign-in.component.ts
@@ -13,14 +13,6 @@ export class SignInComponent implements OnInit {
   ngOnInit(): void {}
 
   submitForm() {
-    // this.username = userName;
-    // this.password = password;
-    // console.log(this.username);
-    // console.log(this.password);
-    // let user = {
-    //   username: this.username,
-    //   password: this.password,
-    // };
     if(this.user.username!=='' && this.user.username!==''){
       localStorage.setItem('user', JSON.stringify(this.user));
     }
