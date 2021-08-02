43b2309d20da6058c4d8f2eb80b43dcdce61fb5e
Amandee Ellawala <amandee.manushika.e@gmail.com>
2021-08-02 17:24:32 +0530

    Add user profile dropdown to the nav-bar

--- conduit-blog-app/src/app/nav-bar/nav-bar.component.ts
+++ conduit-blog-app/src/app/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { UserDetailService } from '../user-detail.service';
+import { UserDetail } from '../model/userDetail';
 
 @Component({
   selector: 'app-nav-bar',
@@ -12,6 +13,8 @@ export class NavBarComponent implements OnInit {
   public isMenuCollapsed = true;
   subscription?: Subscription;
   signedIn: boolean = false;
+  userImage?: string;
+  userName?: string;
   constructor(
     private userDetailService: UserDetailService,
     private router: Router
@@ -21,8 +24,13 @@ export class NavBarComponent implements OnInit {
     this.subscription = this.userDetailService.isSignedIn$.subscribe((data) => {
       this.signedIn = data;
     });
+    this.subscription = this.userDetailService.userDetail$.subscribe((data) => {
+      this.userImage = data?.user.image;
+      this.userName = data?.user.username;
+    });
   }
   logout(event: MouseEvent) {
+    this.isMenuCollapsed = true;
     event.preventDefault();
     this.userDetailService.isSignedInUser(false);
     localStorage.removeItem('token');
